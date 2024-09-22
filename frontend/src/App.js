29cec8bfb54ae822403caa82797509a784229c46
Gauravthkaur <gaurav079t@gmail.com>
2024-09-22 21:54:48 +0530

    first

---
+++ frontend/src/App.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import { HashRouter, Routes, Route, Navigate } from 'react-router-dom';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+import Login from './components/Login';
+import Dashboard from './components/Dashboard';
+import EmployeeList from './components/EmployeeList';
+import CreateEmployee from './components/CreateEmployee';
+import EditEmployee from './components/EditEmployee';
+import Navigation from './components/Navigation';
+
+const theme = createTheme();
+
+// PrivateRoute component moved outside of App
+const PrivateRoute = ({ children }) => {
+  const token = localStorage.getItem('token');
+  return token ? (
+    <>
+      <Navigation />
+      {children}
+    </>
+  ) : (
+    <Navigate to="/" replace />
+  );
+};
+
+function App() {
+  return (
+    <HashRouter>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/dashboard" element={<PrivateRoute><Dashboard /></PrivateRoute>} />
+          <Route path="/employees" element={<PrivateRoute><EmployeeList /></PrivateRoute>} />
+          <Route path="/create-employee" element={<PrivateRoute><CreateEmployee /></PrivateRoute>} />
+          <Route path="/edit-employee/:id" element={<PrivateRoute><EditEmployee /></PrivateRoute>} />
+        </Routes>
+      </ThemeProvider>
+    </HashRouter>
+  );
+}
+
+export default App;
